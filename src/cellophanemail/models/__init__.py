46e097c2b3337a543598f4868f3e9353024a7ba0
youngha kim <yhk@MINIPC-42OBQ.localdomain>
2026-01-30 16:31:18 +1100

    fix: Add missing imports in models/__init__.py

--- src/cellophanemail/models/__init__.py
+++ src/cellophanemail/models/__init__.py
@@ -5,5 +5,20 @@ from .organization import Organization
 from .email_log import EmailLog
 from .subscription import Subscription
 from .shield_address import ShieldAddress
+from .message_analysis import MessageAnalysis, MessageChannel, MessageDirection
+from .sender_summary import SenderSummary
+from .analysis_job import AnalysisJob, JobStatus
 
-__all__ = ["User", "Organization", "EmailLog", "Subscription", "ShieldAddress"]
+__all__ = [
+    "User",
+    "Organization",
+    "EmailLog",
+    "Subscription",
+    "ShieldAddress",
+    "MessageAnalysis",
+    "MessageChannel",
+    "MessageDirection",
+    "SenderSummary",
+    "AnalysisJob",
+    "JobStatus",
+]
